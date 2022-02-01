0ff6f254a64778555e4bf561a6400defa591e19a
Daniel <danielw96@hotmail.com>
2022-02-01 18:51:18 -0500

    small updates

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //should the below commented out code be in calculator?
+    //TextView calculatorCurrentEntry;//example - how to access xml objects in java code
 
     //lifecycle functions / events
     @Override
@@ -13,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("Ass1", "onCreate "+ R.string.app_name);
 
-        Log.d("Ass1", "onCreate "+ R.string.welcome_msg);//how would I set this up with calcCurrentEntry text view? Should I if calculator will be accessing it?
+        //calculatorCurrentEntry = (TextView) findViewById(R.id.calcCurrentEntry);//temp - how to access textView in java code
+
+        //((TextView) findViewById(R.id.calcCurrentEntry)).setText(R.string.welcome_msg);// Should I do this if calculator will be accessing it?
+
+
     }
 
     @Override
