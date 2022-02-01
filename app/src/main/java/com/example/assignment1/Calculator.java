b247ada30785bd2f23e22a3a759e8e2c8c28ca76
Daniel <danielw96@hotmail.com>
2022-02-01 18:51:09 -0500

    started calculator functionality

--- app/src/main/java/com/example/assignment1/Calculator.java
+++ app/src/main/java/com/example/assignment1/Calculator.java
@@ -1,5 +1,40 @@
 package com.example.assignment1;
 
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 public class Calculator {
+
+    //TextView calculatorCurrentEntry;//example - how to access xml objects in java code
+    ArrayList<String> currentEntry;
+
+    void push (String value){
+
+    }
+
+    int calculate() throws ArrayIndexOutOfBoundsException{//does this exception work for valid list size?
+        //checking for valid list size (3 or more, not null/empty)
+        if(currentEntry!=null || currentEntry.isEmpty() || currentEntry.size()<3){
+            try { //does this work instead? xD
+                throw new Exception("Exception message");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //Please click on at least two numbers separated by an operator
+        }
+        else{
+            //for loop running through currentEntry arrayList
+            for (int i=0; currentEntry.size()<i; i++){
+
+
+            }
+
+            return 0;
+        }
+
+        return 0;//needs to return an int to complete :/
+    }
+
 
 }
